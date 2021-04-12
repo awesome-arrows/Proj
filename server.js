078d97d5be964c3c2e5d3f9679867dccca6854ad
NOOR OMARI <nooromari847@gmail.com>
2021-04-12 20:49:28 +0300

    get data from API

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 
 const express = require('express');
 
-
 const pg = require('pg');
 
 const cors = require('cors');
@@ -19,7 +18,9 @@ const client = new pg.Client(DATABASE_URL);
 
 
 //#region Variables Area
-let difficultySelect;
+let diff_string;
+let diff_int;
+
 
 //#endregion
 
@@ -27,7 +28,7 @@ let difficultySelect;
 //#region  Setup
 let app = express();
 
-const PORT = process.env.PORT || 5555;
+const PORT = process.env.PORT || 3005;
 
 // const DATABASE_URL = process.env.DATABASE_URL;
 
@@ -103,7 +104,12 @@ client.on('error', error => console.error(error));
 
 //#region Constructors Area
 
-
+function Question(data){
+    this.question= data.question;
+    this.correct_answer= data.correct_answer;
+    this.incorrect_answers=data.incorrect_answers;
+    this.difficulty=data.difficulty;
+}
 
 
 
@@ -113,26 +119,43 @@ client.on('error', error => console.error(error));
 //#region Functions/Handlers Area
 function renderHomePage(req, res) {
     console.log('homepage is rendering');
-    res.render('pages/index.ejs');
+    res.render('pages/index');
 }
 
 
 function handleQuiz (req,res){
-    console.log(req.body);
-    const userName = [req.body.name];
-    const difficulty=req.body.difficulty;
-    const insertName= 'INSERT INTO users (name) VALUES($1) RETURNING id;';
-    client.query(insertName,userName).then(results=>{ res.redirect('/quiz');
-        console.log('select * from users ;');})
-        .catch(error =>handleError(error,res));
-
-
-    // res.send(req.body);
+    // console.log(req.body);
+    const userName = req.body.name;
+    diff_string=req.body.difficulty;
+    
+    console.log(diff_string);
+    if (diff_string === 'easy') {
+        diff_int = 1;
+    } else if (diff_string === 'medium'){
+        diff_int = 2;
+    } else if (diff_string === 'hard'){
+        diff_int = 3;
+    }
+    const values = [userName];
+    const insertName= 'INSERT INTO users (name) VALUES($1) RETURNING id ;';
+    client.query(insertName,values).then(results=>{ res.redirect('/quiz');})
+    .catch(error =>handleError(error,res));
 }
 
 function handleStart (req,res){
-    console.log('from form', req.body);
-    res.render('pages/quiz-page.ejs');
+    // const diff = req.params.difficulty;
+    console.log('I am start');
+    // const selectDiff= `select difficulty  from quiz_difficulty  where id = ${diff};`;
+
+    // console.log(selectDiff);
+    const url = `https://opentdb.com/api.php?amount=15&category=9&difficulty=${diff_string}&type=multiple`;
+    superagent.get(url)
+        .then(quiz =>{
+            console.log(quiz.body.results);
+            let arr = quiz.body.results.map(ques => new Question(ques));
+            res.render('pages/quiz-page',{data : arr});
+        })
+        .catch(error =>handleError(error,res));
 }
 
 //#endregion
